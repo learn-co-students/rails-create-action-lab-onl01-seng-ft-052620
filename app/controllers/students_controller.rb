d82ef3de4b8c000a78d944f7005d772bc4952d87
aaronfeingold <ajfeingold88@gmail.com>
2020-08-04 17:47:04 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
-  
+  # before_action :set_student, except: [:index, :new, :create]
+
   def index
     @students = Student.all
   end
@@ -9,9 +10,28 @@ class StudentsController < ApplicationController
   end
 
   def new
-  end
-
+    @student = Student.new
+  end 
+  
   def create
+    @student = Student.create(
+          {
+          first_name: params[:first_name],
+          last_name: params[:last_name]
+          }
+        )
+      redirect_to student_path(@student)
   end
-
 end
+  
+  #  private
+
+  #   def set_student
+  #     @student = Student.find_by_id(params[:id])
+  #   end 
+
+  #   def student_params
+  #     params.require(:student).permit(:first_name, :last_name)
+  #   end
+
+
